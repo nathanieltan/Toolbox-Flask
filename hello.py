d117540f1c0a2125bc886442c09f84d431cb4aff
nathanieltan <nathaniel98.tan@gmail.com>
2017-03-04 22:41:28 -0500

    finished toolbox

--- hello.py
+++ hello.py
@@ -3,11 +3,19 @@ Simple "Hello, World" application using Flask
 """
 
 from flask import Flask
+from flask import render_template
+from flask import request
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
-
+    return render_template('index.html')
+@app.route('/test')
+def test():
+    return 'test'
+@app.route('/login', methods = ['POST'])
+def login():
+    return render_template('profile.html', firstName=request.form['firstName'],age=request.form['age'],favoriteNinja='Patrick Huston')
 if __name__ == '__main__':
+    app.debug = True
     app.run()
